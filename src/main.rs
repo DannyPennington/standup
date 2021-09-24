c2157925193039a063dcf1ce3b8d58e4cc6a4bcc
Danny <dannypen@gmail.com>
2021-09-24 17:33:49 +0100

    Nice page formatting & name grouping

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,22 @@ use askama::Template;
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate<'a> {
-    names: Vec<&'a str>
+    grouped_names: Vec<Vec<&'a str>>
 }
 
 async fn index(_req: HttpRequest) -> Result<HttpResponse> {
-    let people: Vec<&str> = vec!["Danny", "Jason", "Jeremy"];
+    let people = vec!["Danny", "Jason", "Jeremy", "Sarah", "Brian", "Baz", "Jen", "Kate", "Glorbo"];
+    let mut grouped_people: Vec<Vec<&str>> = Vec::new();
+    let mut buffer = Vec::new();
+    for index in 0..people.len() {
+        buffer.push(people[index]);
+        if buffer.len() == 4 || index == people.len() - 1 {
+            grouped_people.push(buffer.clone());
+            buffer.clear();
+        }
+    }
     let html = IndexTemplate {
-        names: people
+        grouped_names: grouped_people
     }.render().unwrap();
     println!("Serving html...");
     Ok(HttpResponse::Ok().content_type("text/html").body(html))
