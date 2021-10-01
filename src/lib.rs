43e3b0198cb7fa14fd79f10ea3a959650e38ade5
Daniel Pennington <51288183+DannyPennington@users.noreply.github.com>
2021-10-01 16:17:28 +0100

    Add configuration support (#3)
    * Config setup (broken)
    
    * Working config setup
    
    * Improve frontend and source config via environment variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,36 @@
 pub mod helpers;
+
+use config::{Config, ConfigError};
+
+pub fn basic_grouping(people: Vec<String>) -> Vec<Vec<String>> {
+    let mut grouped_people: Vec<Vec<String>> = vec![];
+    let mut buffer: Vec<String> = vec![];
+    let limit = people.len() - 1;
+    for (index, person) in people.into_iter().enumerate() {
+        buffer.push(person);
+        if buffer.len() == 4 || index == limit {
+            grouped_people.push(buffer.clone());
+            buffer.clear()
+        }
+    }
+    grouped_people
+}
+
+pub fn role_grouping(people: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    let mut result = vec![];
+    for group in people {
+        result.push(basic_grouping(group))
+    }
+    result.into_iter().flatten().collect()
+}
+
+pub fn determine_config() -> Result<Config, ConfigError> {
+    let args: Vec<String> = std::env::args().collect();
+    let mut config = config::Config::default();
+    let name = match args.len() {
+        0 | 1 => {"nw"},
+        _ => {&args[1]}
+    };
+    config.merge(config::File::with_name(name))?;
+    Ok(config)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
-use actix_web::{App, HttpServer, HttpRequest, Result, web, HttpResponse};
+use actix_web::{App, HttpServer, HttpRequest, Result, web, HttpResponse, error};
 use askama::Template;
-use standup::helpers::{role_grouping};
+use standup::helpers::{role_grouping, determine_config};
 use std::env;
+use config::ConfigError;
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate<'a> {
-    grouped_names: Vec<Vec<&'a str>>
+struct IndexTemplate {
+    team_name: String,
+    image_path: String,
+    grouped_names: Vec<Vec<String>>
 }
 
 async fn index(_req: HttpRequest) -> Result<HttpResponse> {
-    // Add people to the relevant list here to add them to the board
-    let devs = vec!["Danny", "Ewan", "Dan", "Sarthak", "Don", "Sean", "Abdi"];
-    let design = vec!["Helen", "Sam", "Shail", "Jacob"];
-    let admin = vec!["Alok", "Maria"];
 
-    let people = vec![devs, design, admin];
-    let grouped_people = role_grouping(people);
+    let error_handler = |e: ConfigError| {
+        error::ErrorInternalServerError(e.to_string())
+    };
+
+    let config = determine_config().map_err(error_handler)?;
+
+    let team_name = config.get_str("team_name").map_err(error_handler)?;
+
+    let image_path = match team_name.as_str() {
+        "Neighbourhood Watch" => "/images/NW-2021.png",
+        "Team Alpha" => "/images/TA-2021.png",
+        _ => "/"
+    }.to_owned();
+
+    let mut groups = vec![];
+    for &group in &["devs", "design", "admin"] {
+        let group_vec: Vec<String> = config.get_array(group).map_err(error_handler)?
+            .into_iter().map(|x| x.into_str().unwrap()).collect();
+        groups.push(group_vec)
+    }
+
+    let grouped_names = role_grouping(groups);
     let html = IndexTemplate {
-        grouped_names: grouped_people
-    }.render().unwrap();
+        team_name,
+        image_path,
+        grouped_names
+    }.render().map_err(|e| {
+        error::ErrorInternalServerError(e.to_string())
+    })?;
 
     println!("Serving html...");
     Ok(HttpResponse::Ok().content_type("text/html").body(html))
@@ -27,13 +50,14 @@ async fn index(_req: HttpRequest) -> Result<HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = env::var("PORT").unwrap_or("4200".to_owned());
+    let port = env::var("PORT").unwrap_or_else(|_| "4200".to_owned());
     println!("Server starting on port {} ...", port);
     HttpServer::new(||
         App::new()
             .service(web::resource("/").route(web::get().to(index)))
             .service(actix_files::Files::new("/js", "./assets/js").show_files_listing())
             .service(actix_files::Files::new("/stylesheets", "./assets/stylesheets").show_files_listing())
+            .service(actix_files::Files::new("/images", "./assets/images").show_files_listing())
     )
         .bind("0.0.0.0:".to_owned() + &*port)?
         .run()
